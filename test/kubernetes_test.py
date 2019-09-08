2a782b5d04653a00e64d5640aed4055b85e9dabd
ksalam28 <54536399+ksalam28@users.noreply.github.com>
2019-09-08 21:25:13 +0200

    Update kubernetes_test.py

--- test/kubernetes_test.py
+++ test/kubernetes_test.py
@@ -22,15 +22,14 @@ class TestKubernetes(unittest.TestCase):
         """
         Test case for log_file_list_handler
         """
-		config.load_kube_config()
-		v1 = client.CoreV1Api()
+	config.load_kube_config()
+	v1 = client.CoreV1Api()
 
-		print("Listing pods with their IPs:")
-		ret = v1.list_pod_for_all_namespaces(watch=False)
-		for i in ret.items:
-			print("%s\t%s\t%s" %
-				  (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-				  
+	print("Listing pods with their IPs:")
+	ret = v1.list_pod_for_all_namespaces(watch=False)
+	for i in ret.items:
+		print("%s\t%s\t%s" %
+			  (i.status.pod_ip, i.metadata.namespace, i.metadata.name))	  
         pass
 
 
