d1b2dfbacfecf087f03aa101ac7c240b6d21c53f
ksalam28 <54536399+ksalam28@users.noreply.github.com>
2019-09-08 12:57:47 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
+import logging
 from flask import Flask
+
+logging.basicConfig(filename='/usr/src/app/file.log', filemode='w',level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/hello')  # this route is not working
 @app.route('/hello/')
 def hello_world():
+	logger.info('hello_world called- output: Hello World')
     return 'Hello World!\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
     # show the user profile for that user
-    return 'Why Hello %s!\n' % username
+	logger.info('hello_user called- output ' + username)
+    return 'Hello %s!\n' % username
 
 if __name__ == '__main__':
+	logger.info('Start Demo Flask app')
     app.run(host='0.0.0.0')  # open for everyone
