17bdfce38ee97a0c3c59521aeebfa6bc5d8b13e2
ksalam28 <54536399+ksalam28@users.noreply.github.com>
2019-09-09 22:25:45 +0200

    Add files via upload

---
+++ test/app_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import unittest
+import requests
+
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_hello(self):
+        rv = requests.get('http://localhost:5000',auth=('user', 'password'))
+        self.assertEqual('{}'.format(rv), '<Response [200]>')
+        self.assertEqual(rv.text, 'Hello World!')
+
+    def test_hello_hello(self):
+        rv = requests.get('http://localhost:5000/hello/',auth=('user', 'password'))
+        self.assertEqual('{}'.format(rv), '<Response [200]>')
+        self.assertEqual(rv.text, 'Hello World!')
+
+    def test_hello_name(self):
+        name = 'Kazi-Alam'
+        rv = requests.get(f'http://localhost:5000/hello/{name}',auth=('user', 'password'))
+        self.assertEqual('{}'.format(rv), '<Response [200]>')
+        #self.assertIn(bytearray(f"Hello {name}", 'utf-8'), rv.text)
+        self.assertEqual(rv.text, 'Hello Kazi-Alam!\n')
+
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    unittest.main()
