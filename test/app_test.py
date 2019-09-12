5752c2749be210ab7b1ff1ac81f1b626ae15af93
Kazi Shafiqul Alam <54536399+ksalam28@users.noreply.github.com>
2019-09-12 12:53:12 +0200

    Update app_test.py

--- test/app_test.py
+++ test/app_test.py
@@ -9,18 +9,27 @@ class TestHello(unittest.TestCase):
         pass
 
     def test_hello(self):
+        print('Testing : test_hello')
         rv = requests.get('http://localhost:5000',auth=('user', 'password'))
+        print(rv)
+        print(tv.text)
         self.assertEqual('{}'.format(rv), '<Response [200]>')
         self.assertEqual(rv.text, 'Hello World!')
 
     def test_hello_hello(self):
+        print('Testing : test_hello_hello')
         rv = requests.get('http://localhost:5000/hello/',auth=('user', 'password'))
+        print(rv)
+        print(tv.text)
         self.assertEqual('{}'.format(rv), '<Response [200]>')
         self.assertEqual(rv.text, 'Hello World!')
 
     def test_hello_name(self):
+        print('Testing : test_hello_name')
         name = 'Kazi-Alam'
         rv = requests.get(f'http://localhost:5000/hello/{name}',auth=('user', 'password'))
+        print(rv)
+        print(tv.text)
         self.assertEqual('{}'.format(rv), '<Response [200]>')
         #self.assertIn(bytearray(f"Hello {name}", 'utf-8'), rv.text)
         self.assertEqual(rv.text, 'Hello Kazi-Alam!\n')
